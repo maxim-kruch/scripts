eba6165075619f9021a76c94c03a502ea226bf70
Maxim Kruch <maximus2004.mk@gmail.com>
2022-12-20 18:41:50 +0100

    jpg-to-pdf-mass.sh geht jetzt

--- jpg-to-pdf-mass.sh
+++ jpg-to-pdf-mass.sh
@@ -1,15 +1,15 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Syntax: jpg-to-pdf-mass.zsh $DATEI_NAME
 
-if [ -z $1 ]
-then
-	echo "Datei angeben!"
-	exit 0
+if [ -z $1 ]; then
+	pdf_file="output.pdf"
 else
+	pdf_file=$1
+fi
 
 for i in *.jpg; do convert "$i" "${i%.*}.pdf"; done
-gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="$1.pdf" *.pdf
-mv "$1.pdf" "$1.pdf.old"
+gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="${pdf_file}" *.pdf
+mv "${pdf_file}" "${pdf_file}.save"
 rm -drf *.pdf
-mv "$1.pdf.old" "$1.pdf"
+mv "${pdf_file}.save" "${pdf_file}"
