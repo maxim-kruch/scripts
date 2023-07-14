eb7c347fb60aa179ec3215dc97d6cf880ca1b11e
maxim-kruch <53115377+maxim-kruch@users.noreply.github.com>
2023-07-14 22:35:52 +0200

    h264 spript

---
+++ mkv-to-h264-cuda.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Syntax: mkv-to-h264-cuda.sh $IN_DIR
+
+if [ -z $1 ]; then
+	indir="."
+else
+	indir=$1
+fi
+
+for i in $indir/*.mkv; do
+	echo ffmpeg -hwaccel cuvid -i "$i" -c:v h264_nvenc -c:a aac "{i%.*}.mp4"
+	ffmpeg -hwaccel cuvid -i "$i" -c:v h264_nvenc -c:a aac "{i%.*}.mp4"
+done
