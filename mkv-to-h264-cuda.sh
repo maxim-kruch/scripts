e2edcf919d7c121f3873b719c7df47f47ddf1f48
maxim-kruch <53115377+maxim-kruch@users.noreply.github.com>
2023-12-25 22:07:24 +0100

    Alle TOnspuren bleiben jetzt erhalten

--- mkv-to-h264-cuda.sh
+++ mkv-to-h264-cuda.sh
@@ -9,6 +9,8 @@ else
 fi
 
 for i in $indir/*.mkv; do
-	echo ffmpeg -hwaccel cuvid -i "$i" -preset slow -c:v h264_nvenc -c:a aac "${i%.*}.mp4"
-	ffmpeg -hwaccel cuvid -i "$i" -preset slow -c:v h264_nvenc -c:a aac "${i%.*}.mp4"
+	#echo ffmpeg -hwaccel cuvid -i "$i" -preset slow -c:v h264_nvenc -c:a aac "${i%.*}.mp4"
+	#ffmpeg -hwaccel cuvid -i "$i" -preset slow -c:v h264_nvenc -c:a aac "${i%.*}.mp4"
+	echo ffmpeg -hwaccel cuvid -i "$i" -preset slow -c:v h264_nvenc -c:a aac -map 0 "${i%.*}.mp4"
+	ffmpeg -hwaccel cuvid -i "$i" -preset slow -c:v h264_nvenc -c:a aac -map 0 "${i%.*}.mp4"
 done
